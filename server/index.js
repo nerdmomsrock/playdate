ebe125a78ec139c6b4bb7bcd52211896fbfc2c51
Ryan Morgan <nerdmomsrock@gmail.com>
2021-07-06 22:28:38 -0400

    set up controllers and worked through endpoints

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {} = require("./authController");
+const {} = require("./matchController");
+const {} = require("profileController");
+
+const app = express();
+
+const PORT = 5050;
+
+app.use(express.json());
+
+app.get("/api/matches/:id", getMatches);
+
+app.get("/api/login", userLogin);
+
+app.post("/api/create", createAccount);
+
+app.put("/api/profile", editProfile);
+
+app.put("/api/matches/:id", rejectMatch);
+
+app.delete("/api/account/:id", deleteAccount);
+
+app.listen(PORT, () => console.log(`Kicking butt on Port ${PORT}!`));
