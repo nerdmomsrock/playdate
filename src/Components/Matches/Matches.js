7b7973f9473e64324c9b6890b72c4d92da8fbc8e
Ryan Morgan <nerdmomsrock@gmail.com>
2021-08-07 00:01:07 -0400

    worked on routes

---
+++ src/Components/Matches/Matches.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Matches() {
+  return (
+    <div className="Matches">
+      <h1>My Matches</h1>
+    </div>
+  );
+}
+
+export default Matches;
---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <h3>PlayDate</h3>
+      <ul>
+        <li>Home</li>
+        <li>Create Account</li>
+        <li>Login</li>
+        <li>Bios</li>
+        <li>My Matches</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
