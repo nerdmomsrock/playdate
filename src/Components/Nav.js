b58ef4180e7d5914959558cb1e1b2b612d9d8f8b
Ryan Morgan <nerdmomsrock@gmail.com>
2021-08-07 12:11:13 -0400

    completed routing

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,15 +1,27 @@
 import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <div className="Nav">
       <h3>PlayDate</h3>
-      <ul>
-        <li>Home</li>
-        <li>Create Account</li>
-        <li>Login</li>
-        <li>Bios</li>
-        <li>My Matches</li>
+      <ul className="nav-links">
+        <Link to="/home">
+          <li>Home</li>
+        </Link>
+        <Link to="/account">
+          <li>Create Account</li>
+        </Link>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+        <Link to="/bios">
+          <li>Bios</li>
+        </Link>
+        <Link to="/matches">
+          <li>My Matches</li>
+        </Link>
       </ul>
     </div>
   );
