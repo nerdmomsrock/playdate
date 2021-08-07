db81d6978b2c8d5b9e5f86d4b7b906328209cdbb
Ryan Morgan <nerdmomsrock@gmail.com>
2021-08-07 12:11:13 -0400

    completed routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Nav from "./Nav";
-import Login from "./Login/Login";
-import Account from "./Account/Account";
-import Bios from "./Bios/Bios";
-import Matches from "./Matches/Matches";
-import Home from "./Home/Home";
-import { HashRouter, Link } from "react-router-dom";
+import Login from "./Login";
+import Account from "./Account";
+import Bios from "./Bios";
+import Matches from "./Matches";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //import routes from './routes';
 
-import "./styles/app.css";
+//import "./styles/app.css";
 
 class App extends Component {
   constructor() {
@@ -17,37 +17,41 @@ class App extends Component {
   }
   render() {
     return (
-      <main>
-        <header>
-          <h1>PlayDate</h1>
-          <h3>Already a Guest?</h3>
-          <button>Login</button>
-          <h3>New Kid on the Block?</h3>
-          <button>Create Account</button>
-        </header>
-        <div className="left">
-          <p>
-            Are you single parenting like a boss, but interested in adding the
-            chaos of dating to your already exhausting life? Sure, there are
-            other dating sites. You've probably spent an annoying amount of
-            hours surfing those, only to be discouraged by the assumption that
-            it's easy for someone like you to find, maintain, and enjoy a
-            relationship. We at PlayDate know just what you need. Enjoy our
-            parent-centered profile builder, and find other single pringles
-            looking to mingle who share your level of sarcasm and inability to
-            participate in an impromptu cocktail hour. We encourage one night
-            stands - if they solely consist of teamwork cleaning out the third
-            row of the minivan and then falling asleep together during the 6:00
-            news. PlayDate....who needs lingerie when you can wear yoga pants?
-          </p>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/account" component={Account} />
+            <Route path="/bios" component={Bios} />
+            <Route path="/matches" component={Matches} />
+          </Switch>
+          <div className="left">
+            <p>
+              Are you single parenting like a boss, but interested in adding the
+              chaos of dating to your already exhausting life? Sure, there are
+              other dating sites. You've probably spent an annoying amount of
+              hours surfing those, only to be discouraged by the assumption that
+              it's easy for someone like you to find, maintain, and enjoy a
+              relationship. We at PlayDate know just what you need. Enjoy our
+              parent-centered profile builder, and find other single pringles
+              looking to mingle who share your level of sarcasm and inability to
+              participate in an impromptu cocktail hour. We encourage one night
+              stands - if they solely consist of teamwork cleaning out the third
+              row of the minivan and then falling asleep together during the
+              6:00 news. PlayDate....who needs lingerie when you can wear yoga
+              pants?
+            </p>
+          </div>
+          <div className="right">
+            <img
+              src="https://media.gettyimages.com/photos/couple-relaxing-on-sofa-while-moving-house-picture-id532097877?s=170667a"
+              alt="exhausted pic"
+            />
+          </div>
         </div>
-        <div className="right">
-          <img
-            src="https://media.gettyimages.com/photos/couple-relaxing-on-sofa-while-moving-house-picture-id532097877?s=170667a"
-            alt="exhausted pic"
-          />
-        </div>
-      </main>
+      </Router>
     );
   }
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,6 +4,28 @@ function Home() {
   return (
     <div className="Home">
       <h1>PlayDate Home</h1>
+      <div className="left">
+        <p>
+          Are you single parenting like a boss, but interested in adding the
+          chaos of dating to your already exhausting life? Sure, there are other
+          dating sites. You've probably spent an annoying amount of hours
+          surfing those, only to be discouraged by the assumption that it's easy
+          for someone like you to find, maintain, and enjoy a relationship. We
+          at PlayDate know just what you need. Enjoy our parent-centered profile
+          builder, and find other single pringles looking to mingle who share
+          your level of sarcasm and inability to participate in an impromptu
+          cocktail hour. We encourage one night stands - if they solely consist
+          of teamwork cleaning out the third row of the minivan and then falling
+          asleep together during the 6:00 news. PlayDate....who needs lingerie
+          when you can wear yoga pants?
+        </p>
+      </div>
+      <div className="right">
+        <img
+          src="https://media.gettyimages.com/photos/couple-relaxing-on-sofa-while-moving-house-picture-id532097877?s=170667a"
+          alt="exhausted pic"
+        />
+      </div>
     </div>
   );
 }
