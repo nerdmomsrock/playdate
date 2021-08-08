6b9e70cec675d6b5f2b823ae59f16f106777613d
Ryan Morgan <nerdmomsrock@gmail.com>
2021-08-07 21:08:58 -0400

    added sass

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,19 +7,19 @@ function Nav() {
     <div className="Nav">
       <h1>PlayDate</h1>
       <ul className="nav-links">
-        <Link to="/home">
+        <Link to="/home" className="link">
           <li>Home</li>
         </Link>
-        <Link to="/account">
+        <Link to="/account" className="link">
           <li>Create Account</li>
         </Link>
-        <Link to="/login">
+        <Link to="/login" className="link">
           <li>Login</li>
         </Link>
-        <Link to="/bios">
+        <Link to="/bios" className="link">
           <li>Bios</li>
         </Link>
-        <Link to="/matches">
+        <Link to="/matches" className="link">
           <li>My Matches</li>
         </Link>
       </ul>
