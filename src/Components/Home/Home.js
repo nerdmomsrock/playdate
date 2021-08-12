1bb319406d5c95eb2ba44fe18278c0a264d4e7b5
Ryan Morgan <nerdmomsrock@gmail.com>
2021-08-12 18:45:18 -0400

    home styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,9 +3,10 @@ import React from "react";
 function Home() {
   return (
     <div className="Home">
+      <br />
       <h1>Welcome To PlayDate!</h1>
       <div className="left">
-        <p>
+        <p className="intro">
           Are you single parenting like a boss, but interested in adding the
           chaos of dating to your already exhausting life? Sure, there are other
           dating sites. You've probably spent an annoying amount of hours
@@ -13,19 +14,22 @@ function Home() {
           for someone like you to find, maintain, and enjoy a relationship. We
           at PlayDate know just what you need. Enjoy our parent-centered profile
           builder, and find other single pringles looking to mingle who share
-          your level of sarcasm and inability to participate in an impromptu
-          cocktail hour. We encourage one night stands - if they solely consist
-          of teamwork cleaning out the third row of the minivan and then falling
-          asleep together during the 6:00 news. PlayDate....who needs lingerie
-          when you can wear yoga pants?
+          your inability to participate in an impromptu cocktail hour. We
+          encourage one night stands - if they solely consist of teamwork
+          cleaning out the third row of the minivan and then falling asleep
+          together during the six o'clock news.
         </p>
       </div>
       <div className="right">
         <img
+          className="homePic"
           src="https://media.gettyimages.com/photos/couple-relaxing-on-sofa-while-moving-house-picture-id532097877?s=170667a"
           alt="exhausted pic"
         />
       </div>
+      <br />
+
+      <h3>PlayDate....who needs lingerie when you can wear yoga pants?</h3>
     </div>
   );
 }
