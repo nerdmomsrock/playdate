3d043c9d7ce3b6f40ba6e9d8fec45f579bb20443
Ryan Morgan <nerdmomsrock@gmail.com>
2021-10-29 23:21:57 -0400

    responsive design all views

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 //import "./App.css"; //comment
 import { Link } from "react-router-dom";
 
 function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => setMenuOpen(!menuOpen);
+
+  const menuClass = menuOpen ? "menu-open" : "";
   return (
     <div className="Nav">
       <h1>#PlayDate</h1>
@@ -23,6 +28,33 @@ function Nav() {
           <li>My Matches</li>
         </Link>
       </ul>
+      <button className="menuButton" onClick={handleClick}>
+        Menu
+        <img
+          className="hamburger"
+          src="http://assets.stickpng.com/images/588a6507d06f6719692a2d15.png"
+          alt="burger-menu"
+        />
+      </button>
+      <menu className={menuClass} style={{ overflow: "hidden" }}>
+        <ul className="menuLinks">
+          <Link to="/home" className="link">
+            <li>Home</li>
+          </Link>
+          <Link to="/account" className="link">
+            <li>Create Account</li>
+          </Link>
+          <Link to="/login" className="link">
+            <li>Login</li>
+          </Link>
+          <Link to="/bios" className="link">
+            <li>Bios</li>
+          </Link>
+          <Link to="/matches" className="link">
+            <li>My Matches</li>
+          </Link>
+        </ul>
+      </menu>
     </div>
   );
 }
