3479b4cb1a9fb4022202e0c40ab5a9ac7a6e714d
Nathaniel Shirts <nshirts7@gmail.com>
2021-10-30 14:03:02 -0600

    routing fixes

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 //import "./App.css"; //comment
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 function Nav(props) {
-  console.log(props, "this is props");
+  console.log(props, 'this is props');
   const [menuOpen, setMenuOpen] = useState(false);
 
   const handleClick = () => setMenuOpen(!menuOpen);
 
-  const menuClass = menuOpen ? "menu-open" : "";
+  const menuClass = menuOpen ? 'menu-open' : '';
   return (
     <div className="Nav">
       <h1>#PlayDate</h1>
       <ul className="nav-links">
-        <Link to="/home" className="link">
+        <Link to="/" className="link">
           <li>Home</li>
         </Link>
         {props.user.email.length > 0 ? null : (
@@ -42,7 +42,7 @@ function Nav(props) {
           alt="burger-menu"
         />
       </button>
-      <menu className={menuClass} style={{ overflow: "hidden" }}>
+      <menu className={menuClass} style={{ overflow: 'hidden' }}>
         <ul className="menuLinks">
           <Link to="/home" className="link">
             <li>Home</li>
